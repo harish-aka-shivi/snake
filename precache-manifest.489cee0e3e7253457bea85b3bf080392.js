6818b48092236c481bf5b06f4f912e153a0cef81
harish <harish.rana@staqu.com>
2019-03-03 14:36:02 +0530

    Updates

---
+++ precache-manifest.489cee0e3e7253457bea85b3bf080392.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da1ecade61dd97107e27",
+    "url": "/snake/static/js/runtime~main.da1ecade.js"
+  },
+  {
+    "revision": "541eb7061a337810a04c",
+    "url": "/snake/static/js/main.541eb706.chunk.js"
+  },
+  {
+    "revision": "11cd304093640138fb65",
+    "url": "/snake/static/js/1.11cd3040.chunk.js"
+  },
+  {
+    "revision": "541eb7061a337810a04c",
+    "url": "/snake/static/css/main.de2d4bc1.chunk.css"
+  },
+  {
+    "revision": "b65add8ea0e4f203d0e7a109091d2c47",
+    "url": "/snake/index.html"
+  }
+];
